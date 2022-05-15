77bb30c297bc445e04d6a9411896b05863ed3a81
nansmatty <narayanmaitysp1997@gmail.com>
2022-05-15 16:38:02 +0530

    es-build test code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useRef, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [input, setInput] = useState<string>('');
+	const [code, setCode] = useState<string>('');
+	const ref = useRef<any>();
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const startService = async () => {
+		ref.current = await esbuild.startService({
+			worker: true,
+			wasmURL: '/esbuild.wasm',
+		});
+	};
+
+	const onClickHandler = async () => {
+		if (!ref.current) {
+			return;
+		}
+		const result = await ref.current.transform(input, {
+			loader: 'jsx',
+			target: 'es2015',
+		});
+
+		setCode(result.code);
+	};
+
+	return (
+		<div>
+			<textarea
+				value={input}
+				rows={10}
+				cols={50}
+				onChange={(e) => setInput(e.target.value)}>
+				Hello World!
+			</textarea>
+			<div>
+				<button onClick={onClickHandler}>Submit</button>
+			</div>
+			<pre>{code}</pre>
+		</div>
+	);
+};
 
 export default App;
